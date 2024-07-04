e78cc21b6872c2a80515105984c838920003d622
Adebanjo Micheal <adebanjo_micheal@yahoo.com>
2024-07-04 19:20:59 +0100

    updated the Time

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//     function updateTime() {
+//         const now = new Date();
+//         const timeString = now.toUTCString().split(' ')[4];
+//         const dayString = now.toLocaleDateString('en-US', { weekday: 'long' });
+
+//         document.getElementById('time').textContent = timeString;
+//         document.getElementById('day').textContent = dayString;
+//     }
+
+//     updateTime();
+//     setInterval(updateTime, 1000); // Update time every minute
+// });
+window.onload = function () {
     function updateTime() {
-        const now = new Date();
-        const timeString = now.toUTCString().split(' ')[4];
-        const dayString = now.toLocaleDateString('en-US', { weekday: 'long' });
-
-        document.getElementById('time').textContent = timeString;
-        document.getElementById('day').textContent = dayString;
+      const now = new Date();
+      document.getElementById("time").innerText = now.toUTCString();
+      document.getElementById("day").innerText = now.toLocaleDateString("en-US", {
+        weekday: "long",
+      });
     }
-
+  
     updateTime();
-    setInterval(updateTime, 1000); // Update time every minute
-});
+    setInterval(updateTime, 1000);
+  };
